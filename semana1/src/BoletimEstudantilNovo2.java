f6773486bba5bdd8b72f8d076d76ee1a91b516c2
Abdiel Athayde <abdieldeathayde67@gmail.com>
2023-01-20 22:08:43 -0300

    atualizando projeto BoletimEstudantil

---
+++ semana1/src/BoletimEstudantilNovo2.java
@@ -0,0 +1,13 @@
+
+
+public class BoletimEstudantilNovo2 {
+	public static void main(String[] args) {
+		int mediaFinal = 7;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+	}
+}
